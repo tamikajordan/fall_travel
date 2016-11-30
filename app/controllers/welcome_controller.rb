1297f35f62563009c476d344574c9b172d51ad7d
Tamika King <tamikajordan@yahoo.com>
2016-11-30 17:03:56 +0000

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+  def index
+    @homestate = 'Georgia'
+    @countries = ["Oman","Croatia","Peru"]
+    
+    @images =["animal.jpg","cold.jpg","deer.jpg","valley.jpg"]
+  
+
+end
+  def about
+    
+    @color = params[:color]
+    @size = params[:size]
+    
+  end
+end
